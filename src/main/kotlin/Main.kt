a5250cf5fe95f3cc76b0099ac2659b0b03d36c7a
2839-23 <klimkina53@gmail.com>
2025-09-06 11:36:39 +0300

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,10 @@
+/*
+// Задача 1
 fun main()
 {
     println ("Программа подсчитывает количество подряд идущих одинаковых символов. \n")
     println("Введите строку:")
-    val input = readLine() ?: ""
+    val input = readln()
     val result = StringBuilder()
     var count = 1
     val length = input.length
@@ -28,4 +30,28 @@ fun main()
         result.append(count)
     }
     println("Вывод результата: $result")
+} */
+
+//Задача 2
+fun main ()
+{
+    println ("Приложение подсчитывает количество различных символов во введенной строке в алфавитном порядке. \n")
+    println ("Введите строку: ")
+    val input = readln()
+    val charCnt = mutableMapOf <Char, Int>()
+
+    for (char in input)
+    {
+        if (charCnt.contrainsKey(char))
+            {
+            charCount[char] = charCnt[char]!! + 1
+            }
+    }
+    val sortedKets = charCnt.key.sorted()
+
+    println ("Результат: ")
+    for (char in sortedKeys)
+    {
+        println ("#char-@{charCnt[char]}")
+    }
 }
