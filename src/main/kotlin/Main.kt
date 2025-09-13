9df600ddee613f5e6895b6875b68dd6260138048
2839-23 <klimkina53@gmail.com>
2025-09-13 10:18:18 +0300

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -30,8 +30,7 @@ fun main()
         result.append(count)
     }
     println("Вывод результата: $result")
-} */
-
+}
 //Задача 2
 fun main ()
 {
@@ -42,16 +41,100 @@ fun main ()
 
     for (char in input)
     {
-        if (charCnt.contrainsKey(char))
+        if (charCnt.containsKey(char))
             {
-            charCount[char] = charCnt[char]!! + 1
+                charCnt[char] = charCnt[char]!! + 1
             }
+        else
+        {
+            charCnt[char]=1
+        }
     }
-    val sortedKets = charCnt.key.sorted()
-
+    val sortedKeys = charCnt.keys.sorted()
     println ("Результат: ")
     for (char in sortedKeys)
     {
-        println ("#char-@{charCnt[char]}")
+        println ("$char-${charCnt[char]}")
+    }
+}
+
+//Задача 3
+fun main ()
+{
+    println("Приложение, которое преобразует введенное пользователем натуральное число из 10-ичной системы в двоичную.\n")
+    println("Введите натуральное число из 10-ичной системы счисления: ")
+    val input = readln()
+    if (input.all { it.isDigit() }&& input.isNotEmpty())
+    {
+        val num = input.toInt()
+        if (num == 0)
+        {
+            println("Результат: 0")
+        }
+        else {
+            val numb = num.toString(2)
+            println("Результат преобразования: ")
+            println("Десятичное число: $num")
+            println("Двоичное число: $numb")
+        }
+    }
+    else
+    {
+        println("Число введено не корректно! \n Введите корректное число!")
+    }
+}
+*/
+//Задача 4
+fun main()
+{
+    println("Приложение простого калькулятора.\n")
+    println("Введите выражение в формате - ЧИСЛО1 ЧИСЛО2 ОПЕРАЦИЯ: ")
+    val input = readln().trim()
+    val parts = input.split(" ")
+    if (parts.size != 3)
+    {
+        println("Ошибка: введите выражение в формате ЧИСЛО1 ЧИСЛО2 ОПЕРАЦИЯ")
+        return
+    }
+    val number1 = parts[0].toDoubleOrNull()
+    val number2 = parts[1].toDoubleOrNull()
+    val operation = parts[2]
+    if (number1 == null || number2 == null)
+    {
+        println("Ошибка: введите корректные числа")
+        return
+    }
+    when (operation)
+    {
+        "+" ->
+        {
+            val result = number1 + number2
+            println("Результат: $number1 + $number2 = $result")
+        }
+        "-" ->
+        {
+            val result = number1 - number2
+            println("Результат: $number1 - $number2 = $result")
+        }
+        "*" ->
+        {
+            val result = number1 * number2
+            println("Результат: $number1 * $number2 = $result")
+        }
+        "/" ->
+        {
+            if (number2 == 0.0)
+            {
+                println("Ошибка: деление на ноль невозможно")
+            } else
+            {
+                val result = number1 / number2
+                println("Результат: $number1 / $number2 = $result")
+            }
+        }
+        else ->
+        {
+            println("Ошибка: неизвестная операция '$operation'. Доступные операции: + - * /")
+        }
     }
 }
