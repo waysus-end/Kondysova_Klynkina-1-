a0c0df9c25ce471ce8bbaf2bdd5c3f81d215f5ac
2839-23 <klimkina53@gmail.com>
2025-09-13 11:30:34 +0300

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,47 @@
-/*
-// Задача 1
 fun main()
+{
+    while (true)
+    {
+        println("╔═══════════════════════════════╗")
+        println("║ МЕНЮ ПРОГРАММ                 ║")
+        println("╠═══════════════════════════════╣")
+        println("║ 1. Задача 1                   ║")
+        println("║ 2. Задача 2                   ║")
+        println("║ 3. Задача 3                   ║")
+        println("║ 4. Задача 4                   ║")
+        println("║ 5. Задача 5                   ║")
+        println("║ 6. Задача 6                   ║")
+        println("║ 0. Выход                      ║")
+        println("╚═══════════════════════════════╝")
+
+        print("Выберите программу (0-6): \n")
+        val choice = readln().toIntOrNull()
+
+        when (choice)
+        {
+            0 ->
+            {
+                println("Выход из программы.")
+                return
+            }
+            1 -> program1()
+            2 -> program2()
+            3 -> program3()
+            4 -> program4()
+            5 -> program5()
+            6 -> program6()
+            else -> println("Ошибка: выберите число от 0 до 6")
+        }
+
+        println("\nНажмите Enter для продолжения...\n")
+        readln()
+    }
+}
+
+
+
+// Задача 1
+fun program1()
 {
     println ("Программа подсчитывает количество подряд идущих одинаковых символов. \n")
     println("Введите строку:")
@@ -32,7 +73,7 @@ fun main()
     println("Вывод результата: $result")
 }
 //Задача 2
-fun main ()
+fun program2()
 {
     println ("Приложение подсчитывает количество различных символов во введенной строке в алфавитном порядке. \n")
     println ("Введите строку: ")
@@ -59,7 +100,7 @@ fun main ()
 }
 
 //Задача 3
-fun main ()
+fun program3()
 {
     println("Приложение, которое преобразует введенное пользователем натуральное число из 10-ичной системы в двоичную.\n")
     println("Введите натуральное число из 10-ичной системы счисления: ")
@@ -83,9 +124,9 @@ fun main ()
         println("Число введено не корректно! \n Введите корректное число!")
     }
 }
-*/
+
 //Задача 4
-fun main()
+fun program4()
 {
     println("Приложение простого калькулятора.\n")
     println("Введите выражение в формате - ЧИСЛО1 ЧИСЛО2 ОПЕРАЦИЯ: ")
@@ -138,3 +179,130 @@ fun main()
         }
     }
 }
+//Задача 5
+fun program5()
+{
+    println("Приложение, с помощью которого пользователь, введя целое " +
+            "число n и основание степени x узнает, существует ли целочисленный показатель " +
+            "степени y для которого выполняется равенство xy = n. \n")
+    println("Введите целое число n: ")
+    val ninp = readln()
+    val n= ninp.toIntOrNull()
+    if (n==null)
+    {
+        println("Ошибка: введите корректное целое число")
+        return
+    }
+    print("Введите основание степени x: ")
+    val xinp = readln()
+    val X=xinp.toIntOrNull()
+    if (X == null)
+    {
+        println("Ошибка: введите корректное основание степени")
+        return
+    }
+    if (X==0||X==1)
+    {
+        if (n==1)
+        {
+            println("Показатель степени y=0 ")
+        }
+        else if (X==0 && n==0)
+        {
+            println("Показатель степени y=1 ")
+        }
+        else
+        {
+            println("Целочисленного показателя не существует")
+        }
+        return
+    }
+    if (n==0)
+    {
+        println("Показатель степени y=0 ")
+        return
+    }
+    if (n==0)
+    {
+        if (X==0)
+        {
+            println("Показатель степени y=1 ")
+        }
+        else
+        {
+            println("Целочисленного показателя не существует")
+        }
+        return
+    }
+    var y=0
+    var power=1
+
+    while (true)
+    {
+        if (power == n)
+        {
+            println ("Показатель степени y = $y")
+            return
+        }
+
+        if (power > n || power < n && power == 0) {
+            break
+        }
+        val nextPow = power * X
+        if (nextPow / X!= power)
+        {
+            break
+        }
+
+        power = nextPow
+        y++
+    }
+    println ("Целочисленного показателя не существует")
+}
+//Задача 6
+fun program6()
+{
+    println("Приложение, в котором пользователь вводит две различных цифры. " +
+                "На выходе приложение выдает, если это возможно, из введенных цифр, нечетное число. \n"
+    )
+    println("Введите первую цифру: ")
+    val digit1 = readln().toIntOrNull()
+    print("Введите вторую цифру: ")
+    val digit2 = readln().toIntOrNull()
+    if (digit1 == null || digit2 == null)
+    {
+        println("Ошибка: введите корректные цифры (0-9)")
+        return
+    }
+
+    if (digit1 !in 0..9 || digit2 !in 0..9)
+    {
+        println("Ошибка: введите цифры от 0 до 9")
+        return
+    }
+
+    if (digit1 == digit2)
+    {
+        println("Ошибка: цифры должны быть различны")
+        return
+    }
+
+    val number1 = digit1 * 10 + digit2
+    val number2 = digit2 * 10 + digit1
+
+    val oddNumber = when
+    {
+        number1 % 2 != 0 -> number1
+        number2 % 2 != 0 -> number2
+        else -> null
+    }
+
+
+    if (oddNumber != null)
+    {
+        println("Нечетное число: $oddNumber")
+    }
+    else
+    {
+        println("Создать нечетное число невозможно")
+    }}
